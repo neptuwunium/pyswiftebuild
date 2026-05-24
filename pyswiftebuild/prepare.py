65f57aa77fb6c64ddd3cef84e75f66a3e16f316f
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-24 13:43:45 +0530

    fix: prepare

--- pyswiftebuild/prepare.py
+++ pyswiftebuild/prepare.py
@@ -11,16 +11,18 @@ def construct_build_env(workdir: str):
 	dependencies = []
 
 	for dependency in swiftpm.get_dependencies(workdir):
+		data = dependency.data
+		version = dependency.version
 		dependencies.append({
 			'basedOn': None,
 			'packageRef': {
-				'identity': dependency.data['identity'],
-				'kind': dependency.data['kind'],
-				'location': dependency.data['location'],
-				'name': dependency.data['identity'],
+				'identity': data['identity'] if 'identity' in data else data['package'].lower(),
+				'kind': data['kind'] if 'kind' in data else 'remoteSourceControl',
+				'location': data['location'] if 'location' in data else data['repositoryURL'],
+				'name': data['package'] if 'package' in data else data['identity'],
 			},
 			'state': {
-				'checkoutState': dependency.data['state'],
+				'checkoutState': data['state'],
 				'name': 'sourceControlCheckout'
 			},
 			'subpath': f'{dependency.name}-{dependency.state.revision}'
@@ -29,7 +31,7 @@ def construct_build_env(workdir: str):
 	workspaceState = {
 		'object': {
 			'artifacts': [],
-			'dependencies': dependencies
+			'dependencies': dependencies,
 		},
 		'version': 6
 	}
